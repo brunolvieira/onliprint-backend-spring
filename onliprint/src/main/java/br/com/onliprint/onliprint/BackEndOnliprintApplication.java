06ae4c0b9229c1b46bd72483d9aa5a80c151b80a
Bruno Laurindo Vieira <51804080+brunolvieira@users.noreply.github.com>
2022-03-09 23:07:15 -0300

    feat: First commit adding the created project

---
+++ onliprint/src/main/java/br/com/onliprint/onliprint/BackEndOnliprintApplication.java
@@ -0,0 +1,13 @@
+package br.com.onliprint.onliprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndOnliprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndOnliprintApplication.class, args);
+	}
+
+}
