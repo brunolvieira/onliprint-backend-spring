8a2e5d676a8383c71a4addfc232c1b1123288a28
Brunolvieira <bruno.l.vieira@hotmail.com>
2022-09-24 02:32:11 -0300

    feat: first commit

---
+++ src/main/java/com/onliprint/onliprintbackendspring/OnliprintBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package com.onliprint.onliprintbackendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnliprintBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnliprintBackendSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onliprint/onliprintbackendspring/OnliprintBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onliprint.onliprintbackendspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnliprintBackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
